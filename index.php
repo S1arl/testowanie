839b90e580015c9906cc65435a4c008272b19eb2
S1arl <wyganowskikarol@gmail.com>
2016-02-18 01:35:29 +0100

    All ok

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+ class A {
+        public $a = 1;
+          public function pierwsza() {
+              if($this->a == 1) {
+                  throw new myException();
+              }
+          }
+         
+      }
+     
+     
+      
+     $v = new A();
+     try{
+     $v->pierwsza();
+     }
+     catch(Exception $e) {
+         echo $e->errorMessage();
+     }
+     
+     class myException extends Exception {
+         public function errorMessage() {
+             return "HELLO";
+         }
+         
+     }
+        ?>
+    </body>
+</html>
